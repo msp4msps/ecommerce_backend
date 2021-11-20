80f188265fccd1246eb668e41ab06c99947c1c01
msp4msps <msp4msps@tminus365.com>
2021-11-20 08:08:39 -0700

    add associations

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,50 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 
+Product.hasOne(Category, {
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
+});
+
+Category.belongsTo(Product, {
+  foreignKey: "product_id",
+});
+
 // Categories have many Products
+
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
+
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "tags",
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "product_tags",
+});
 
 module.exports = {
   Product,
