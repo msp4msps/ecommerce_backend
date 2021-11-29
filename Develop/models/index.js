086c3f7cbcd520add658af6cacb6d8b9fe2b7587
msp4msps <msp4msps@tminus365.com>
2021-11-29 08:37:25 -0700

    update tag routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,48 +4,32 @@ const Category = require("./Category");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
-
-Product.hasOne(Category, {
-  foreignKey: "product_id",
-  onDelete: "CASCADE",
-});
-
-Category.belongsTo(Product, {
-  foreignKey: "product_id",
-});
-
-// Categories have many Products
-
-Category.hasMany(Product, {
-  foreignKey: "category_id",
-  onDelete: "CASCADE",
-});
-
 Product.belongsTo(Category, {
   foreignKey: "category_id",
+  onDelete: "CASCADE",
 });
 
-// Products belongToMany Tags (through ProductTag)
-
-Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false,
-  },
-  as: "tags",
+// Categories have many Products  Has Many: Product holds foreign key
+Category.hasMany(Product, {
+  foreignKey: "category_id",
 });
+// Products belongToMany Tags (
+Product.belongsToMany(
+  Tag,
+  { through: ProductTag },
+  {
+    foreignKey: "ProductTag_id",
+  }
+);
 
 // Tags belongToMany Products (through ProductTag)
-
-Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false,
-  },
-  as: "product_tags",
-});
-
+Tag.belongsToMany(
+  Product,
+  { through: ProductTag },
+  {
+    foreignKey: "Tag_id",
+  }
+);
 module.exports = {
   Product,
   Category,
